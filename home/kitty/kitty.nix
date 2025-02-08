8789c933fd09579347b3b00c0f8865c0d4a78717
Ojas Kavathe <ojaskavathe@gmail.com>
2025-02-08 19:13:42 +0530

    fix: don't change kitty logo
    Kitty logo override not working on macos

--- home/kitty/kitty.nix
+++ home/kitty/kitty.nix
@@ -12,27 +12,28 @@
   };
 
   config = lib.mkIf config.kitty.enable {
-    nixpkgs = {
-      overlays = [
-        # change icon
-        (final: prev: {
-          kitty = prev.kitty.overrideAttrs (oldAttrs: {
-            postInstall =
-              (oldAttrs.postInstall or "")
-              + ''
-                cp -f ${./kitty.app.png} $out/share/icons/hicolor/256x256/apps/kitty.png
-                rm -f $out/share/icons/hicolor/scalable/apps/kitty.svg
-              '';
-          });
-        })
-
-      ];
-    };
+    # nixpkgs = {
+    #   overlays = [
+    #     # change icon
+    #     (final: prev: {
+    #       kitty = prev.kitty.overrideAttrs (oldAttrs: {
+    #         postInstall =
+    #           (oldAttrs.postInstall or "")
+    #           + ''
+    #             cp -f ${./kitty.app.png} $out/share/icons/hicolor/256x256/apps/kitty.png
+    #             rm -f $out/share/icons/hicolor/scalable/apps/kitty.svg
+    #           '';
+    #       });
+    #     })
+    #
+    #   ];
+    # };
 
     programs.kitty = {
       enable = true;
       settings = {
         confirm_os_window_close = 0;
+        # hide_window_decorations = "titlebar-only";
       };
     };
   };
