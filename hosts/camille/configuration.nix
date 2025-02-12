7efbe0077ee7a2699f47959bf93171d2045d980c
Ojas Kavathe <ojaskavathe@gmail.com>
2025-02-12 17:14:00 +0530

    feat: macos stuff

--- hosts/camille/configuration.nix
+++ hosts/camille/configuration.nix
@@ -26,15 +26,40 @@
 
   security.pam.enableSudoTouchIdAuth = true;
 
+  # activationScripts are executed every time you boot the system or run `nixos-rebuild` / `darwin-rebuild`.
+  system.activationScripts = {
+    postUserActivation.text = ''
+      # activateSettings -u will reload the settings from the database and apply them to the current session,
+      # so we do not need to logout and login again to make the changes take effect.
+      /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+    '';
+  };
+
   system.defaults = {
+    menuExtraClock.Show24Hour = true;
+
+    loginwindow = {
+      GuestEnabled = false; # disable guest user
+    };
+
     dock = {
       autohide = true;
       static-only = true;
+      show-recents = false;
       tilesize = 32;
     };
 
+    finder = {
+      _FXShowPosixPathInTitle = true; # show full path in finder title
+      AppleShowAllExtensions = true; # show all file extensions
+      FXEnableExtensionChangeWarning = false; # disable warning when changing file extension
+      QuitMenuItem = true; # enable quit menu item
+      ShowPathbar = true; # show path bar
+      ShowStatusBar = true; # show status bar
+    };
+
     NSGlobalDomain = {
-      # ctrl + cmd + mouse to drag windows  
+      # ctrl + cmd + mouse to drag windows
       NSWindowShouldDragOnGesture = true;
 
       InitialKeyRepeat = 25;
