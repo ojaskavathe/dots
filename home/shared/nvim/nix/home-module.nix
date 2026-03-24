27d53f021fa276bd1b946a3bb8c6f47270f47c75
Ojas Kavathe <ojaskavathe@gmail.com>
2026-03-25 00:49:51 +0530

    chore: flake upgrade

--- home/shared/nvim/nix/home-module.nix
+++ home/shared/nvim/nix/home-module.nix
@@ -34,7 +34,7 @@ in
 
     package = mkOption {
       type = types.package;
-      default = self.packages.${pkgs.system}.default;
+      default = self.packages.${pkgs.stdenv.hostPlatform.system}.default;
       description = "The neovim package to use";
     };
 
