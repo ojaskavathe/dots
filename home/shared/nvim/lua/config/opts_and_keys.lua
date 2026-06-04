b8301b4837b8607f4ef8a5484c826f85dd01998d
Ojas Kavathe <ojaskavathe@gmail.com>
2026-06-04 11:31:35 +0530

    feat: unescape

--- home/shared/nvim/lua/config/opts_and_keys.lua
+++ home/shared/nvim/lua/config/opts_and_keys.lua
@@ -146,6 +146,55 @@ end, { desc = "Yank relative path with line" })
 -- Select all
 vim.keymap.set("n", "<leader>sa", "ggVG", { desc = "Select all" })
 
+local function unescape_text(text)
+	text = text:gsub("\\r\\n", "\n")
+	text = text:gsub("\\u([dD][89aAbB]%x%x)\\u([dD][c-fC-F]%x%x)", function(high, low)
+		local high_code = tonumber(high, 16)
+		local low_code = tonumber(low, 16)
+		local codepoint = 0x10000 + (high_code - 0xD800) * 0x400 + (low_code - 0xDC00)
+		return vim.fn.nr2char(codepoint, true)
+	end)
+	text = text:gsub("\\u(%x%x%x%x)", function(hex)
+		local codepoint = tonumber(hex, 16)
+		if codepoint >= 0xD800 and codepoint <= 0xDFFF then
+			return "\\u" .. hex
+		end
+		return vim.fn.nr2char(codepoint, true)
+	end)
+
+	local escapes = {
+		['"'] = '"',
+		["'"] = "'",
+		["\\"] = "\\",
+		["/"] = "/",
+		b = "\b",
+		f = "\f",
+		n = "\n",
+		r = "\n",
+		t = "\t",
+	}
+
+	return (text:gsub("\\(.)", function(char)
+		return escapes[char] or "\\" .. char
+	end))
+end
+
+vim.api.nvim_create_user_command("UnescapeText", function(opts)
+	local lines = vim.api.nvim_buf_get_lines(0, opts.line1 - 1, opts.line2, false)
+	if #lines == 0 then
+		return
+	end
+
+	local text = table.concat(lines, "\n")
+	local unescaped = unescape_text(text)
+	local new_lines = vim.split(unescaped, "\n", { plain = true })
+
+	vim.api.nvim_buf_set_lines(0, opts.line1 - 1, opts.line2, false, new_lines)
+end, { range = "%", desc = "Unescape backslash text" })
+
+vim.keymap.set("n", "<leader>su", "<cmd>UnescapeText<CR>", { desc = "Unescape text" })
+vim.keymap.set("x", "<leader>su", ":UnescapeText<CR>", { desc = "Unescape text" })
+
 -- Tab keymaps
 vim.keymap.set("n", "<leader><Tab>n", "<cmd>tabnew<cr>", { desc = "New tab" })
 vim.keymap.set("n", "<leader><Tab>c", "<cmd>tabclose<cr>", { desc = "Close tab" })
